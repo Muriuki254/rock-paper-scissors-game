afbe8a314e988ba0d07c59636bcc430d326d4748
hum-dev <humphreymuriungi@outlook.com>
2023-05-25 12:04:35 +0300

    added comments

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -6,18 +6,18 @@ import Score from "./Score";
 import Outcome from "./Outcome";
 
 const Game = () => {
-  const [pick, setPick] = useState("");
+  const [pick, setPick] = useState(""); //useState hook to manage the user's pick (rock, paper, or scissors).
   const [score, setScore] = useState({
     player: 0,
     computer: 0,
-  });
-  const [computerPick, setComputerPick] = useState("");
+  }); //useState hook to manage the game score, with initial values of 0 for both the player and the computer.
+  const [computerPick, setComputerPick] = useState(""); //useState hook to store the computer's pick.
 
-  const handlePick = (pick) => {
+  const handlePick = (pick) => { //Updates the pick state variable when the user selects a pick.
     setPick(pick);
   };
 
-  const playAgain = () => {
+  const playAgain = () => {  //Resets the pick state to an empty string and score state to the initial values 
     setPick("");
     setScore({
       player: 0,
@@ -25,16 +25,16 @@ const Game = () => {
     });
   };
 
-  useEffect(() => {
-    const randomPick = Math.floor(Math.random() * 3);
+  useEffect(() => {  //Triggers when the pick state changes.
+    const randomPick = Math.floor(Math.random() * 3);  //Generates a random pick for the computer
     const pickOptions = ["rock", "paper", "scissors"];
-    const newComputerPick = pickOptions[randomPick];
+    const newComputerPick = pickOptions[randomPick]; //Updates the computerPick state variable with the new computer pick.
     setComputerPick(newComputerPick);
   }, [pick]);
 
-  useEffect(() => {
+  useEffect(() => {  //Triggers when either the pick or the computerPick state changes.
     if (pick !== "") {
-      const outcome = checkOutcome(pick, computerPick);
+      const outcome = checkOutcome(pick, computerPick); //Checks the outcome of the game by calling the checkOutcome function with the player's pick and the computer's pick.
       setScore((prevScore) => {
         if (outcome === "Player Wins") {
           return { ...prevScore, player: prevScore.player + 1 };
